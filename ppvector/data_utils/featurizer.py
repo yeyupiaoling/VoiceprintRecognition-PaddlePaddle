e98073d506c1ac5f7ea03bad50211830fddf1178
yeyupiaoling <yeyupiaoling@foxmail.com>
2023-02-25 11:37:12 +0800

    更新默认参数

--- ppvector/data_utils/featurizer.py
+++ ppvector/data_utils/featurizer.py
@@ -65,6 +65,6 @@ class AudioFeaturizer(nn.Layer):
         elif self._feature_method == 'Spectrogram':
             return 257
         elif self._feature_method == 'MFCC':
-            return self._feature_conf.n_mels
+            return self._feature_conf.n_mfcc
         else:
             raise Exception('没有{}预处理方法'.format(self._feature_method))
